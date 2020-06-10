9837be747ff3b0e3228edc59a770389390cb4db8
liuenci <liuenci@cvte.com>
2020-06-10 17:46:11 +0800

    [init] 初始化工程

---
+++ src/main/java/com/liuencier/code/tookit/TookitApplication.java
@@ -0,0 +1,13 @@
+package com.liuencier.code.tookit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TookitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TookitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liuencier/code/tookit/TookitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liuencier.code.tookit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TookitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
